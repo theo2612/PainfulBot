a41e71714790904dcbba467f0360128936e39972
Theo <brad.theodore@gmail.com>
2025-11-06 11:43:05 -0500

    modified generateAccessToken.sh and player_data.json

--- generateUserAccessToken.sh
+++ generateUserAccessToken.sh
@@ -1,5 +1,5 @@
 https://id.twitch.tv/oauth2/authorize
-?client_id=ekgic4k98yb2mvhxs22heqtq2lis4p
+?client_id=zh85893gpdttjc6zyuvj8rtoijzg48
 &redirect_uri=https://localhost
 &response_type=code
 &scope=chat:read+chat:edit+channel:moderate+channel:read:subscriptions+user:read:email
